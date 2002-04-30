7bb5c1b4e21a36843f1504952896f6e9734cab24
William E. Kempf <wekempf@cox.net>
2002-04-30 21:27:21 +0000

    Modified barrier implementation
    [SVN r13597]

--- include/boost/thread/barrier.hpp
+++ include/boost/thread/barrier.hpp
@@ -1,23 +1,13 @@
-// Copyright (C)  2002
-// David Moore
-//
-// Original mutex and cv design and implementation for Boost.Threads
-// Copyright (C) 2001
-// William E. Kempf
+// Copyright (C) 2002
+// David Moore, William E. Kempf
 //
 // Permission to use, copy, modify, distribute and sell this software
 // and its documentation for any purpose is hereby granted without fee,
 // provided that the above copyright notice appear in all copies and
 // that both that copyright notice and this permission notice appear
-// in supporting documentation.  David Moore makes no representations
+// in supporting documentation.  William E. Kempf makes no representations
 // about the suitability of this software for any purpose.
 // It is provided "as is" without express or implied warranty.
-//
-//
-// A Boost.Threads implementation of a synchronization 
-//   primitive which allows a fixed number of threads to synchronize
-//   their execution paths at selected "barriers"
-
 
 #ifndef BOOST_RW_MUTEX_JDM030602_HPP
 #define BOOST_RW_MUTEX_JDM030602_HPP
@@ -25,63 +15,26 @@
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
 
-
 namespace boost { 
 
-    class barrier;
-
-    namespace detail { namespace thread {
-    class sub_barrier
-    {
-    public:
-        sub_barrier(unsigned int count);
-        ~sub_barrier();
-    private:
-        unsigned int m_running;
-        boost::condition m_barrier_finished;
-
-        friend class boost::barrier;
-    };
-    } // namespace thread
-    } // namespace detail
-
-const int BOOST_SERIAL_THREAD = -1;
+class barrier;
 
 class barrier
 {
 public:
     barrier(unsigned int count);
-    int wait();
-    ~barrier();     
+    ~barrier();
+
+    bool wait();
+
 private:
-
-    unsigned int m_generation;
-    unsigned int m_count;
-    detail::thread::sub_barrier m_sub_1;
-    detail::thread::sub_barrier m_sub_2;
-    detail::thread::sub_barrier *m_subs[2];
-
     mutex m_mutex;
-};
-
-
-
-
-class one_shot_barrier 
-{
-public:
-
-    one_shot_barrier(unsigned int count);
-    ~one_shot_barrier();
-
-    int wait();
-private:
-    boost::condition m_barrier_finished;
-    boost::mutex m_mutex;
-    unsigned int m_running;     // How many threads running?
+	condition m_cond;
+    unsigned int m_threshold;
+    unsigned int m_count;
+	unsigned int m_generation;
 };
 
 }   // namespace boost
-
 
 #endif
--- src/barrier.cpp
+++ src/barrier.cpp
@@ -1,154 +1,45 @@
-// Copyright (C)  2002
-// David Moore
-//
-// Original mutex and cv design and implementation for Boost.Threads
-// Copyright (C) 2001
-// William E. Kempf
+// Copyright (C) 2002
+// David Moore, William E. Kempf
 //
 // Permission to use, copy, modify, distribute and sell this software
 // and its documentation for any purpose is hereby granted without fee,
 // provided that the above copyright notice appear in all copies and
 // that both that copyright notice and this permission notice appear
-// in supporting documentation.  David Moore makes no representations
+// in supporting documentation.  William E. Kempf makes no representations
 // about the suitability of this software for any purpose.
 // It is provided "as is" without express or implied warranty.
-//
-//
-// A Boost.Threads implementation of a synchronization 
-//   primitive which allows a fixed number of threads to synchronize
-//   their execution paths at selected "barriers"
-
 
 #include <boost/thread/barrier.hpp>
 
-
 namespace boost {
 
-
-
-barrier::
-barrier(unsigned int count) : 
-    m_count(count), m_generation(0), 
-    m_sub_1(count), m_sub_2(count)
+barrier::barrier(unsigned int count)
+    : m_threshold(count), m_count(count), m_generation(0)
 {
-    if(count == 0)
+    if (count == 0)
         throw std::invalid_argument("count cannot be zero.");
-
-    m_subs[0] = &m_sub_1;
-    m_subs[1] = &m_sub_2;
 }
 
-barrier::
-~barrier()
+barrier::~barrier()
 {   
-    boost::mutex::scoped_lock l(m_mutex);
-    // This is our "signal" that we've been destroyed.
-    m_subs[0] = m_subs[1] = NULL;
 }
 
-
-int
-barrier::
-wait()
+bool barrier::wait()
 {
-    int ret;
-    boost::mutex::scoped_lock l(m_mutex);
+    boost::mutex::scoped_lock lock(m_mutex);
+	unsigned int gen = m_generation;
 
-    detail::thread::sub_barrier *psub = m_subs[m_generation];
-    if(psub == NULL)
-        throw lock_error();
+	if (--m_count == 0)
+	{
+		m_generation++;
+		m_count = m_threshold;
+		m_cond.notify_all();
+		return true;
+	}
 
-    if(psub->m_running == 1)
-    {
-        // This is the last running thread for the current
-        // generation.
-
-        // reset that generation's count to our full count for next time
-        psub->m_running = m_count;
-        m_generation = 1-m_generation;
-        psub->m_barrier_finished.notify_all();
-        ret = BOOST_SERIAL_THREAD;        // We're the one who woke them up
-    }
-    else 
-    {
-        // decrement the count for that generation
-        --psub->m_running;
-        // wait to see the condition where we are "reset" by the 
-        //    last waiter...
-        while(psub->m_running != m_count)
-            psub->m_barrier_finished.wait(l);
-        ret = 0;        // We were notified to wake up
-    }
-
-    return ret;
+	while (gen == m_generation)
+		m_cond.wait(lock);
+	return false;
 }
 
-
-namespace detail { namespace thread {
-sub_barrier::
-sub_barrier(unsigned int count) :
-    m_running(count)
-{
-
-}	
-
-sub_barrier::
-~sub_barrier()
-{
-}
-
-} // namespace thread
-} // namespace detail
-
-
-
-one_shot_barrier::
-one_shot_barrier(unsigned int count) : m_running(count)
-{
-}
-
-one_shot_barrier::
-~one_shot_barrier()
-{
-}
-
-
-int 
-one_shot_barrier::
-wait()
-{
-    int ret;
-    boost::mutex::scoped_lock l(m_mutex);
-
-    if(m_running == 1)
-    {
-        // we are the last waiting thread.
-        ret = BOOST_SERIAL_THREAD;
-        m_running = 0;
-        m_barrier_finished.notify_all();
-    }
-    else if(m_running != 0)
-    {
-        --m_running;
-
-        while(m_running != 0)
-            m_barrier_finished.wait(l);
-        ret = 0;
-    }
-    else
-    {
-        // wait has been called "too many" times.
-        //
-        // For this simple barrier, we don't support generations
-        //   of waiting, so there are two choices here - ignore and allow
-        //   the caller to proceed, or throw an exception..
-        //
-        throw lock_error();
-    }
-    return ret;
-}
-
-    
-}// namespace boost
-
-
+} // namespace boost
