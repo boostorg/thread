a084bad83d31563250f6ad336baaa6abc79cdcec
Dave Abrahams <dave@boostpro.com>
2003-08-21 22:51:38 +0000

    Port "conditin.hpp" typo fix over from Version_1_30_2

--- include/boost/thread.hpp
+++ include/boost/thread.hpp
@@ -13,7 +13,7 @@
 #define BOOST_THREAD_WEK01082003_HPP
 
 #include <boost/thread/thread.hpp>
-#include <boost/thread/conditin.hpp>
+#include <boost/thread/condition.hpp>
 #include <boost/thread/exceptions.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/once.hpp>
