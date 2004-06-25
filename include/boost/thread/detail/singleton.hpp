5995ef22e71ff23a61989d74b0a137b01a3e4e88
Michael Glassford <glassfordm@hotmail.com>
2004-06-25 20:22:02 +0000

    Cleanup: always #include <boost/thread/detail/config.hpp> first; eliminate tabs; etc.

--- include/boost/thread/detail/singleton.hpp
+++ include/boost/thread/detail/singleton.hpp
@@ -14,6 +14,8 @@
 #ifndef BOOST_SINGLETON_MJM012402_HPP
 #define BOOST_SINGLETON_MJM012402_HPP
 
+#include <boost/thread/detail/config.hpp>
+
 namespace boost {
 namespace detail {
 namespace thread {
--- src/mutex.cpp
+++ src/mutex.cpp
@@ -9,6 +9,8 @@
 // about the suitability of this software for any purpose.
 // It is provided "as is" without express or implied warranty.
 
+#include <boost/thread/detail/config.hpp>
+
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/xtime.hpp>
 #include <boost/thread/thread.hpp>
