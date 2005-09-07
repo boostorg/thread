4fd15423b56b21ef7eaa0a6919630186d0ec58ea
Anthony Williams <anthony.ajw@gmail.com>
2005-09-07 15:02:16 +0000

    New version of call_once for win32

--- include/boost/thread/once.hpp
+++ include/boost/thread/once.hpp
@@ -8,6 +8,8 @@
 // in supporting documentation.  William E. Kempf makes no representations
 // about the suitability of this software for any purpose.
 // It is provided "as is" without express or implied warranty.
+//
+// (C) Copyright 2005 Anthony Williams
 
 #ifndef BOOST_ONCE_WEK080101_HPP
 #define BOOST_ONCE_WEK080101_HPP
@@ -18,6 +20,12 @@
 #   include <pthread.h>
 #endif
 
+#if defined(BOOST_HAS_WINTHREADS) 
+
+#include <boost/thread/detail/once_win32.hpp>
+
+#else
+
 namespace boost {
 
 #if defined(BOOST_HAS_PTHREADS)
@@ -25,18 +33,16 @@ namespace boost {
 typedef pthread_once_t once_flag;
 #define BOOST_ONCE_INIT PTHREAD_ONCE_INIT
 
-#elif (defined(BOOST_HAS_WINTHREADS) || defined(BOOST_HAS_MPTASKS))
-
-typedef long once_flag;
-#define BOOST_ONCE_INIT 0
-
 #endif
 
 void BOOST_THREAD_DECL call_once(void (*func)(), once_flag& flag);
 
 } // namespace boost
 
+#endif
+
 // Change Log:
 //   1 Aug 01  WEKEMPF Initial version.
+//   6 Sep 05  Anthony Williams. Split win32 stuff into detail/once_win32.hpp
 
 #endif // BOOST_ONCE_WEK080101_HPP
