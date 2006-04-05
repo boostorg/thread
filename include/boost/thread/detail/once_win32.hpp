3c9be720b88ec9037758d3adbf5caaab7366426a
Anthony Williams <anthony.ajw@gmail.com>
2006-04-05 11:42:07 +0000

    Simplified code by extracting create_once_mutex, so the essence of call_once is clarified. Use boost::detail::interlocked_read rather than BOOST_INTERLOCKED_COMPARE_EXCHANGE to check flag value
    [SVN r33553]

--- include/boost/thread/detail/once_win32.hpp
+++ include/boost/thread/detail/once_win32.hpp
@@ -16,11 +16,14 @@
 #include <boost/static_assert.hpp>
 #include <boost/detail/interlocked.hpp>
 #include <boost/thread/detail/win32_thread_primitives.hpp>
+#include <boost/thread/detail/interlocked_read_win32.hpp>
 
 #ifdef BOOST_NO_STDC_NAMESPACE
 namespace std
 {
     using ::strlen;
+    using ::memcpy;
+    using ::ptrdiff_t;
 }
 #endif
 
@@ -68,39 +71,48 @@ namespace boost
             }
             buf[i] = 0;
         }
+
+        unsigned const once_mutex_name_fixed_length=48;
+        unsigned const once_mutex_name_fixed_buffer_size=once_mutex_name_fixed_length+1;
+        unsigned const once_mutex_name_length=once_mutex_name_fixed_buffer_size+sizeof(void*)*2+sizeof(unsigned long)*2;
+        
+        // create a named mutex. It doesn't really matter what this name is
+        // as long as it is unique both to this process, and to the address of "flag":
+        inline void* create_once_mutex(char (&mutex_name)[once_mutex_name_length],void* flag_address)
+        {
+            static const char fixed_mutex_name[]="{C15730E2-145C-4c5e-B005-3BC753F42475}-once-flag";
+            BOOST_STATIC_ASSERT(sizeof(fixed_mutex_name) == once_mutex_name_fixed_buffer_size);
+
+            std::memcpy(mutex_name,fixed_mutex_name,sizeof(fixed_mutex_name));
+            BOOST_ASSERT(sizeof(mutex_name) == std::strlen(mutex_name) + sizeof(void*)*2 + sizeof(unsigned long)*2 + 1);
+
+            BOOST_STATIC_ASSERT(sizeof(void*) == sizeof(std::ptrdiff_t));
+            detail::int_to_string(reinterpret_cast<std::ptrdiff_t>(flag_address), mutex_name + once_mutex_name_fixed_length);
+            detail::int_to_string(BOOST_GET_PROCESS_ID(), mutex_name + once_mutex_name_fixed_length + sizeof(void*)*2);
+            BOOST_ASSERT(sizeof(mutex_name) == std::strlen(mutex_name) + 1);
+
+            return BOOST_CREATE_MUTEX(NULL, 0, mutex_name);
+        }
+        
     }
     
 
     template<typename Function>
     void call_once(Function f,once_flag& flag)
     {
-        //
         // Try for a quick win: if the proceedure has already been called
         // just skip through:
-        //
         long const function_complete_flag_value=0xc15730e2;
 
-        if(BOOST_INTERLOCKED_COMPARE_EXCHANGE(&flag,function_complete_flag_value,
-                                              function_complete_flag_value)!=function_complete_flag_value)
+        if(::boost::detail::interlocked_read(&flag)!=function_complete_flag_value)
         {
-            //
-            // create a name for our mutex, it doesn't really matter what this name is
-            // as long as it is unique both to this process, and to the address of "flag":
-            //
-            char mutex_name[49+sizeof(void*)*2+sizeof(unsigned long)*2] = { "{C15730E2-145C-4c5e-B005-3BC753F42475}-once-flag" };
-            BOOST_ASSERT(sizeof(mutex_name) == std::strlen(mutex_name) + sizeof(void*)*2 + sizeof(unsigned long)*2 + 1);
-            BOOST_STATIC_ASSERT(sizeof(void*) == sizeof(std::ptrdiff_t));
-            detail::int_to_string(reinterpret_cast<std::ptrdiff_t>(&flag), mutex_name + 48);
-            detail::int_to_string(BOOST_GET_PROCESS_ID(), mutex_name + 48 + sizeof(void*)*2);
-            BOOST_ASSERT(sizeof(mutex_name) == std::strlen(mutex_name) + 1);
-
-            void* const mutex_handle(BOOST_CREATE_MUTEX(NULL, 0, mutex_name));
+            char mutex_name[::boost::detail::once_mutex_name_length];
+            void* const mutex_handle(::boost::detail::create_once_mutex(mutex_name,&flag));
             BOOST_ASSERT(mutex_handle);
             detail::handle_closer const closer(mutex_handle);
             detail::win32_mutex_scoped_lock const lock(mutex_handle);
       
-            if(BOOST_INTERLOCKED_COMPARE_EXCHANGE(&flag,function_complete_flag_value,
-                                                  function_complete_flag_value)!=function_complete_flag_value)
+            if(::boost::detail::interlocked_read(&flag)!=function_complete_flag_value)
             {
                 f();
                 BOOST_INTERLOCKED_EXCHANGE(&flag,function_complete_flag_value);
