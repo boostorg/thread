45ca67ec26eea836ed90118c5ee3b2007e655037
Roland Schwarz <roland.schwarz@blackspace.at>
2006-04-11 22:23:47 +0000

    obsolete code
    [SVN r33672]

--- src/mac/prefix.hpp
+++
@@ -1,9 +0,0 @@
-// (C) Copyright Mac Murrett 2001.
-// Use, modification and distribution are subject to the 
-// Boost Software License, Version 1.0. (See accompanying file 
-// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-// See http://www.boost.org for most recent version.
-
-#define NDEBUG
-#define TARGET_CARBON    1
