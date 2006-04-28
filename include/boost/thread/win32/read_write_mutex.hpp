71c652dd9f553fc4b29616f642c1a8d91908a53d
Anthony Williams <anthony.ajw@gmail.com>
2006-04-28 08:20:18 +0000

    Basic condition-based read-write mutex
    [SVN r33850]

--- include/boost/thread/win32/read_write_mutex.hpp
+++ include/boost/thread/win32/read_write_mutex.hpp
@@ -1,17 +1,68 @@
 #ifndef BOOST_THREAD_WIN32_READ_WRITE_MUTEX_HPP
 #define BOOST_THREAD_WIN32_READ_WRITE_MUTEX_HPP
 
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
+
 namespace boost
 {
     class read_write_mutex
     {
+    private:
+        boost::mutex guard;
+        boost::condition cond;
+        long reader_count;
+        
     public:
+        read_write_mutex():
+            reader_count(0)
+        {}
+        
         class scoped_read_lock
         {
+            read_write_mutex& m;
         public:
-            scoped_read_lock(read_write_mutex&)
-            {}
+            scoped_read_lock(read_write_mutex& m_):
+                m(m_)
+            {
+                boost::mutex::scoped_lock lock(m.guard);
+                ++m.reader_count;
+            }
+            ~scoped_read_lock()
+            {
+                boost::mutex::scoped_lock lock(m.guard);
+                --m.reader_count;
+                m.cond.notify_one();
+            }
+        };
+
+        class scoped_write_lock
+        {
+            read_write_mutex& m;
+            boost::mutex::scoped_lock lock;
             
+        public:
+            scoped_write_lock(read_write_mutex& m_):
+                m(m_),
+                lock(m.guard)
+            {
+                while(m.reader_count)
+                {
+                    m.cond.wait(lock);
+                }
+            }
+            void unlock()
+            {
+                m.cond.notify_one();
+                lock.unlock();
+            }
+            ~scoped_write_lock()
+            {
+                if(lock.locked())
+                {
+                    unlock();
+                }
+            }
         };
         
     };
--- test/test_read_write_mutex.cpp
+++ test/test_read_write_mutex.cpp
@@ -82,6 +82,122 @@ void test_multiple_readers()
     pool.join_all();
 }
 
+void test_only_one_writer_permitted()
+{
+    unsigned const number_of_threads=100;
+    
+    boost::thread_group pool;
+
+    boost::read_write_mutex rw_mutex;
+    unsigned unblocked_count=0;
+    boost::mutex unblocked_count_mutex;
+    boost::mutex finish_mutex;
+    boost::mutex::scoped_lock finish_lock(finish_mutex);
+    
+    for(unsigned i=0;i<number_of_threads;++i)
+    {
+        pool.create_thread(locking_thread<boost::read_write_mutex::scoped_write_lock>(rw_mutex,unblocked_count,unblocked_count_mutex,finish_mutex));
+    }
+
+    boost::thread::sleep(delay(1));
+
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,1U);
+
+    finish_lock.unlock();
+
+    pool.join_all();
+
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,number_of_threads);
+}
+
+void test_reader_blocks_writer()
+{
+    boost::thread_group pool;
+
+    boost::read_write_mutex rw_mutex;
+    unsigned unblocked_count=0;
+    boost::mutex unblocked_count_mutex;
+    boost::mutex finish_mutex;
+    boost::mutex::scoped_lock finish_lock(finish_mutex);
+    
+    pool.create_thread(locking_thread<boost::read_write_mutex::scoped_read_lock>(rw_mutex,unblocked_count,unblocked_count_mutex,finish_mutex));
+    boost::thread::sleep(delay(1));
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,1U);
+    pool.create_thread(locking_thread<boost::read_write_mutex::scoped_write_lock>(rw_mutex,unblocked_count,unblocked_count_mutex,finish_mutex));
+    boost::thread::sleep(delay(1));
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,1U);
+
+    finish_lock.unlock();
+
+    pool.join_all();
+
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,2U);
+}
+
+void test_unlocking_writer_unblocks_all_readers()
+{
+    boost::thread_group pool;
+
+    boost::read_write_mutex rw_mutex;
+    boost::read_write_mutex::scoped_write_lock write_lock(rw_mutex);
+    unsigned unblocked_count=0;
+    boost::mutex unblocked_count_mutex;
+    boost::mutex finish_mutex;
+    boost::mutex::scoped_lock finish_lock(finish_mutex);
+
+    unsigned const reader_count=100;
+
+    for(unsigned i=0;i<reader_count;++i)
+    {
+        pool.create_thread(locking_thread<boost::read_write_mutex::scoped_read_lock>(rw_mutex,unblocked_count,unblocked_count_mutex,finish_mutex));
+    }
+    boost::thread::sleep(delay(1));
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,0U);
+
+    write_lock.unlock();
+    
+    boost::thread::sleep(delay(1));
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,reader_count);
+
+    finish_lock.unlock();
+    pool.join_all();
+}
+
+void test_unlocking_last_reader_only_unblocks_one_writer()
+{
+    boost::thread_group pool;
+
+    boost::read_write_mutex rw_mutex;
+    unsigned unblocked_count=0;
+    boost::mutex unblocked_count_mutex;
+    boost::mutex finish_reading_mutex;
+    boost::mutex::scoped_lock finish_reading_lock(finish_reading_mutex);
+    boost::mutex finish_writing_mutex;
+    boost::mutex::scoped_lock finish_writing_lock(finish_writing_mutex);
+
+    unsigned const reader_count=100;
+    unsigned const writer_count=100;
+
+    for(unsigned i=0;i<reader_count;++i)
+    {
+        pool.create_thread(locking_thread<boost::read_write_mutex::scoped_read_lock>(rw_mutex,unblocked_count,unblocked_count_mutex,finish_reading_mutex));
+    }
+    for(unsigned i=0;i<writer_count;++i)
+    {
+        pool.create_thread(locking_thread<boost::read_write_mutex::scoped_write_lock>(rw_mutex,unblocked_count,unblocked_count_mutex,finish_writing_mutex));
+    }
+    boost::thread::sleep(delay(1));
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,reader_count);
+
+    finish_reading_lock.unlock();
+
+    boost::thread::sleep(delay(1));
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,reader_count+1);
+
+    finish_writing_lock.unlock();
+    pool.join_all();
+    CHECK_LOCKED_VALUE_EQUAL(unblocked_count_mutex,unblocked_count,reader_count+writer_count);
+}
 
 boost::unit_test_framework::test_suite* init_unit_test_suite(int, char*[])
 {
@@ -89,6 +205,10 @@ boost::unit_test_framework::test_suite* init_unit_test_suite(int, char*[])
         BOOST_TEST_SUITE("Boost.Threads: read_write_mutex test suite");
 
     test->add(BOOST_TEST_CASE(&test_multiple_readers));
+    test->add(BOOST_TEST_CASE(&test_only_one_writer_permitted));
+    test->add(BOOST_TEST_CASE(&test_reader_blocks_writer));
+    test->add(BOOST_TEST_CASE(&test_unlocking_writer_unblocks_all_readers));
+    test->add(BOOST_TEST_CASE(&test_unlocking_last_reader_only_unblocks_one_writer));
 
     return test;
 }
