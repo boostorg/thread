11178cdc84371326ad8fcb99f2493157a6add2c1
Anthony Williams <anthony.ajw@gmail.com>
2006-06-08 14:40:43 +0000

    Templated test_initially_locked, and applied to boost::try_mutex::scoped_lock too
    [SVN r34240]

--- test/test_lock_concept.cpp
+++ test/test_lock_concept.cpp
@@ -6,14 +6,18 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/thread/mutex.hpp>
 
-void test_mutex_scoped_lock_initially_locked()
+template<typename Mutex,typename Lock>
+struct test_initially_locked
 {
-    boost::mutex m;
-    boost::mutex::scoped_lock lock(m);
-    
-    BOOST_CHECK(lock);
-    BOOST_CHECK(lock.locked());
-}
+    void operator()() const
+    {
+        Mutex m;
+        Lock lock(m);
+        
+        BOOST_CHECK(lock);
+        BOOST_CHECK(lock.locked());
+    }
+};
 
 void test_mutex_scoped_lock_initially_locked_with_bool_parameter_true()
 {
@@ -73,7 +77,8 @@ boost::unit_test_framework::test_suite* init_unit_test_suite(int, char*[])
     boost::unit_test_framework::test_suite* test =
         BOOST_TEST_SUITE("Boost.Threads: lock concept test suite");
 
-    test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_initially_locked));
+    test->add(BOOST_TEST_CASE((test_initially_locked<boost::mutex,boost::mutex::scoped_lock>())));
+    test->add(BOOST_TEST_CASE((test_initially_locked<boost::try_mutex,boost::try_mutex::scoped_lock>())));
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_initially_locked_with_bool_parameter_true));
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_initially_unlocked_with_bool_parameter_false));
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_unlocked_after_unlock_called));
