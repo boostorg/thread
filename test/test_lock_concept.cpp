54d37cd6af13b89229baea5501e14cf6b2e09d73
Anthony Williams <anthony.ajw@gmail.com>
2006-06-08 14:33:29 +0000

    Added more checks round scoped_lock concept
    [SVN r34239]

--- test/test_lock_concept.cpp
+++ test/test_lock_concept.cpp
@@ -15,19 +15,46 @@ void test_mutex_scoped_lock_initially_locked()
     BOOST_CHECK(lock.locked());
 }
 
+void test_mutex_scoped_lock_initially_locked_with_bool_parameter_true()
+{
+    boost::mutex m;
+    boost::mutex::scoped_lock lock(m,true);
+    
+    BOOST_CHECK(lock);
+    BOOST_CHECK(lock.locked());
+}
+
+void test_mutex_scoped_lock_initially_unlocked_with_bool_parameter_false()
+{
+    boost::mutex m;
+    boost::mutex::scoped_lock lock(m,false);
+    
+    BOOST_CHECK(!lock);
+    BOOST_CHECK(!lock.locked());
+}
+
 void test_mutex_scoped_lock_unlocked_after_unlock_called()
 {
     boost::mutex m;
-    boost::mutex::scoped_lock lock(m);
+    boost::mutex::scoped_lock lock(m,true);
     lock.unlock();
     BOOST_CHECK(!lock);
     BOOST_CHECK(!lock.locked());
 }
 
+void test_mutex_scoped_lock_locked_after_lock_called()
+{
+    boost::mutex m;
+    boost::mutex::scoped_lock lock(m,false);
+    lock.lock();
+    BOOST_CHECK(lock);
+    BOOST_CHECK(lock.locked());
+}
+
 void test_mutex_scoped_lock_throws_if_lock_called_when_already_locked()
 {
     boost::mutex m;
-    boost::mutex::scoped_lock lock(m);
+    boost::mutex::scoped_lock lock(m,true);
     
     BOOST_CHECK_THROW( lock.lock(), boost::lock_error );
 }
@@ -35,7 +62,7 @@ void test_mutex_scoped_lock_throws_if_lock_called_when_already_locked()
 void test_mutex_scoped_lock_throws_if_unlock_called_when_already_unlocked()
 {
     boost::mutex m;
-    boost::mutex::scoped_lock lock(m);
+    boost::mutex::scoped_lock lock(m,true);
     lock.unlock();
     
     BOOST_CHECK_THROW( lock.unlock(), boost::lock_error );
@@ -47,7 +74,10 @@ boost::unit_test_framework::test_suite* init_unit_test_suite(int, char*[])
         BOOST_TEST_SUITE("Boost.Threads: lock concept test suite");
 
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_initially_locked));
+    test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_initially_locked_with_bool_parameter_true));
+    test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_initially_unlocked_with_bool_parameter_false));
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_unlocked_after_unlock_called));
+    test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_locked_after_lock_called));
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_throws_if_lock_called_when_already_locked));
     test->add(BOOST_TEST_CASE(&test_mutex_scoped_lock_throws_if_unlock_called_when_already_unlocked));
 
