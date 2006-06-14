8ce25c6ecd92241eb008d4d09e750533d6d94dd3
Anthony Williams <anthony.ajw@gmail.com>
2006-06-14 07:48:05 +0000

    Changed scoped_lock_test to templated test case, in order to get better error messages
    [SVN r34299]

--- test/test_lock_concept.cpp
+++ test/test_lock_concept.cpp
@@ -4,6 +4,8 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+#include <boost/mpl/vector.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/recursive_mutex.hpp>
 
@@ -99,18 +101,17 @@ struct test_throws_if_unlock_called_when_already_unlocked
     }
 };
 
-template<typename Mutex>
-void add_tests_for_scoped_lock_concept(boost::unit_test_framework::test_suite* test,Mutex* =0)
+BOOST_TEST_CASE_TEMPLATE_FUNCTION(test_scoped_lock_concept,Mutex)
 {
     typedef typename Mutex::scoped_lock Lock;
     
-    test->add(BOOST_TEST_CASE((test_initially_locked<Mutex,Lock>())));
-    test->add(BOOST_TEST_CASE((test_initially_locked_with_bool_parameter_true<Mutex,Lock>())));
-    test->add(BOOST_TEST_CASE((test_initially_unlocked_with_bool_parameter_false<Mutex,Lock>())));
-    test->add(BOOST_TEST_CASE((test_unlocked_after_unlock_called<Mutex,Lock>())));
-    test->add(BOOST_TEST_CASE((test_locked_after_lock_called<Mutex,Lock>())));
-    test->add(BOOST_TEST_CASE((test_throws_if_lock_called_when_already_locked<Mutex,Lock>())));
-    test->add(BOOST_TEST_CASE((test_throws_if_unlock_called_when_already_unlocked<Mutex,Lock>())));
+    test_initially_locked<Mutex,Lock>()();
+    test_initially_locked_with_bool_parameter_true<Mutex,Lock>()();
+    test_initially_unlocked_with_bool_parameter_false<Mutex,Lock>()();
+    test_unlocked_after_unlock_called<Mutex,Lock>()();
+    test_locked_after_lock_called<Mutex,Lock>()();
+    test_throws_if_lock_called_when_already_locked<Mutex,Lock>()();
+    test_throws_if_unlock_called_when_already_unlocked<Mutex,Lock>()();
 }
 
 template<typename Mutex>
@@ -132,13 +133,11 @@ boost::unit_test_framework::test_suite* init_unit_test_suite(int, char*[])
     boost::unit_test_framework::test_suite* test =
         BOOST_TEST_SUITE("Boost.Threads: lock concept test suite");
 
-    add_tests_for_scoped_lock_concept<boost::mutex>(test);
-    add_tests_for_scoped_lock_concept<boost::try_mutex>(test);
-    add_tests_for_scoped_lock_concept<boost::timed_mutex>(test);
-    add_tests_for_scoped_lock_concept<boost::recursive_mutex>(test);
-    add_tests_for_scoped_lock_concept<boost::recursive_try_mutex>(test);
-    add_tests_for_scoped_lock_concept<boost::recursive_timed_mutex>(test);
-
+    typedef boost::mpl::vector<boost::mutex,boost::try_mutex,boost::timed_mutex,
+        boost::recursive_mutex,boost::recursive_try_mutex,boost::recursive_timed_mutex> mutex_types;
+    
+    test->add(BOOST_TEST_CASE_TEMPLATE(test_scoped_lock_concept,mutex_types));
+    
     add_tests_for_scoped_try_lock_concept<boost::try_mutex>(test);
     add_tests_for_scoped_try_lock_concept<boost::recursive_try_mutex>(test);
 
