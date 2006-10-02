3a7e569a653a39328aeff594407aca740819ad76
Roland Schwarz <roland.schwarz@blackspace.at>
2006-10-02 07:49:04 +0000

    Test if this turns regressions green on win x64 platforms
    [SVN r35437]

--- src/once.cpp
+++ src/once.cpp
@@ -113,7 +113,14 @@ inline LONG ice_wrapper(LPVOID (__stdcall *ice)(LPVOID*, LPVOID, LPVOID),
 // according to the above function type wrappers.
 inline LONG compare_exchange(volatile LPLONG dest, LONG exch, LONG cmp)
 {
+#ifdef _WIN64
+    // Original patch from Anthony Williams.
+    // I (Roland Schwarz) am trying this for RC_1_34_0, since x64 regressions are
+    // currently not run on x64 platforms for HEAD
+    return InterlockedCompareExchange(dest, exch,cmp);
+#else    
     return ice_wrapper(&InterlockedCompareExchange, dest, exch, cmp);
+#endif
 }
 }
 #endif
