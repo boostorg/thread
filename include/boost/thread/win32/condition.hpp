ab0e9930c5aa8994a692d2a949426e662ba3b2e1
Anthony Williams <anthony.ajw@gmail.com>
2007-03-15 13:53:43 +0000

    Added copyright and licence
    [SVN r37189]

--- include/boost/thread/win32/condition.hpp
+++ include/boost/thread/win32/condition.hpp
@@ -1,3 +1,7 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// (C) Copyright 2007 Anthony Williams
 #ifndef CONDITION_HPP
 #define CONDITION_HPP
 #include "boost/config.hpp"
