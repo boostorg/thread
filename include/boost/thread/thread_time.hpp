c0dbcf94d07ee3c2d31bddd49e8f2cdaf2b9d56b
Anthony Williams <anthony.ajw@gmail.com>
2007-11-07 12:10:17 +0000

    Added missing licence and copyright

--- include/boost/thread/thread_time.hpp
+++ include/boost/thread/thread_time.hpp
@@ -1,5 +1,11 @@
 #ifndef BOOST_THREAD_TIME_HPP
 #define BOOST_THREAD_TIME_HPP
+//  (C) Copyright 2007 Anthony Williams 
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/date_time/microsec_time_clock.hpp>
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 
