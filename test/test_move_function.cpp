77d5576c96f1bb3b712e73dadaa645133e7a7943
Anthony Williams <anthony.ajw@gmail.com>
2007-12-17 12:52:50 +0000

    boost::move support for locks

--- test/test_move_function.cpp
+++ test/test_move_function.cpp
@@ -4,6 +4,7 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/thread/thread.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/thread/mutex.hpp>
 
 void do_nothing()
 {}
@@ -19,7 +20,7 @@ void test_thread_move_from_lvalue_on_construction()
     dest.join();
 }
 
-void test_thread_move_from_rvalue()
+void test_thread_move_from_rvalue_on_construction()
 {
     boost::thread x(boost::move(boost::thread(do_nothing)));
     BOOST_CHECK(x.get_id()!=boost::thread::id());
@@ -27,12 +28,27 @@ void test_thread_move_from_rvalue()
 }
 
 
+void test_unique_lock_move_from_lvalue_on_construction()
+{
+    boost::mutex m;
+    boost::unique_lock<boost::mutex> l(m);
+    BOOST_CHECK(l.owns_lock());
+    BOOST_CHECK(l.mutex()==&m);
+
+    boost::unique_lock<boost::mutex> l2(boost::move(l));
+    BOOST_CHECK(!l.owns_lock());
+    BOOST_CHECK(!l.mutex());
+    BOOST_CHECK(l2.owns_lock());
+    BOOST_CHECK(l2.mutex()==&m);
+}
+
 boost::unit_test_framework::test_suite* init_unit_test_suite(int, char*[])
 {
     boost::unit_test_framework::test_suite* test =
         BOOST_TEST_SUITE("Boost.Threads: thread move test suite");
 
     test->add(BOOST_TEST_CASE(test_thread_move_from_lvalue_on_construction));
-    test->add(BOOST_TEST_CASE(test_thread_move_from_rvalue));
+    test->add(BOOST_TEST_CASE(test_thread_move_from_rvalue_on_construction));
+    test->add(BOOST_TEST_CASE(test_unique_lock_move_from_lvalue_on_construction));
     return test;
 }
