7afd9efcc544a73b47b1ec32bd3576c9ca0fd536
Anthony Williams <anthony.ajw@gmail.com>
2007-12-20 08:37:02 +0000

    added hardware_concurrency support for apple, freebsd and sun platforms
    [SVN r42195]

--- src/pthread/thread.cpp
+++ src/pthread/thread.cpp
@@ -15,6 +15,11 @@
 #include <boost/thread/tss.hpp>
 #ifdef __linux__
 #include <sys/sysinfo.h>
+#elif defined(__APPLE__) || defined(__FreeBSD__)
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#elif defined(__sun)
+#include <unistd.h>
 #endif
 
 #include "timeconv.inl"
@@ -415,6 +420,13 @@ namespace boost
         return pthread_num_processors_np();
 #elif defined(__linux__)
         return get_nprocs();
+#elif defined(__APPLE__) || defined(__FreeBSD__)
+        int count;
+        size_t size=sizeof(count);
+        return sysctlbyname("hw.ncpu",&count,&size,NULL,0)?0:count;
+#elif defined(__sun)
+        int const count=sysconf(_SC_NPROCESSORS_ONLN);
+        return (count>0)?count:0;
 #else
         return 0;
 #endif
