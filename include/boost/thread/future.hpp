5ac5426ff71bdc75a2f258906f5565c1d96f8c88
Anthony Williams <anthony.ajw@gmail.com>
2010-06-10 08:22:39 +0000

    Remove warnings. Fix for issue #3611

--- include/boost/thread/future.hpp
+++ include/boost/thread/future.hpp
@@ -142,6 +142,8 @@ namespace boost
                 {
                     lock.lock();
                 }
+            private:
+                relocker& operator=(relocker const&);
             };
 
             void do_callback(boost::unique_lock<boost::mutex>& lock)
