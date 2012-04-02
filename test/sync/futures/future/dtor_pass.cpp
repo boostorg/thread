298b51fefa0c00611f9555d04f3432d5cc3e4902
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-04-02 22:49:11 +0000

    Thread: Check if adding BOOST_EXPLICIT_MOVE helps on Sun
    [SVN r77729]

--- test/sync/futures/future/dtor_pass.cpp
+++ test/sync/futures/future/dtor_pass.cpp
@@ -37,7 +37,7 @@ int main()
       {
           boost::promise<T> p(boost::allocator_arg, test_allocator<T>());
           BOOST_TEST(test_alloc_base::count == 1);
-          f = p.get_future();
+          f = BOOST_EXPLICIT_MOVE(p.get_future());
           BOOST_TEST(test_alloc_base::count == 1);
           BOOST_TEST(f.valid());
       }
@@ -51,7 +51,7 @@ int main()
       {
           boost::promise<T> p(boost::allocator_arg, test_allocator<int>());
           BOOST_TEST(test_alloc_base::count == 1);
-          f = p.get_future();
+          f = BOOST_EXPLICIT_MOVE(p.get_future());
           BOOST_TEST(test_alloc_base::count == 1);
           BOOST_TEST(f.valid());
       }
@@ -65,7 +65,7 @@ int main()
       {
           boost::promise<T> p(boost::allocator_arg, test_allocator<T>());
           BOOST_TEST(test_alloc_base::count == 1);
-          f = p.get_future();
+          f = BOOST_EXPLICIT_MOVE(p.get_future());
           BOOST_TEST(test_alloc_base::count == 1);
           BOOST_TEST(f.valid());
       }
