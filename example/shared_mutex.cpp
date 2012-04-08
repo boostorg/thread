8a1342ec647b2e3a4425d6eca3d145f80bb400e0
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-04-08 20:56:34 +0000

    Thread: Fix type

--- example/shared_mutex.cpp
+++ example/shared_mutex.cpp
@@ -3,9 +3,9 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define BOOST_THREAD_SHARED_MUTEX_PROVIDES_UPWARDS_CONVERSION
+#define BOOST_THREAD_PROVIDES_SHARED_MUTEX_UPWARDS_CONVERSIONS
 #define BOOST_THREAD_PROVIDES_EXPLICIT_LOCK_CONVERSION
-#define BOOST_THREAD_SHARED_MUTEX_GENERIC
+#define BOOST_THREAD_DONT_PROVIDE_GENERIC_SHARED_MUTEX_ON_WIN
 
 #include <iostream>
 #include <boost/thread/mutex.hpp>
