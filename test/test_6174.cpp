1ac7e7129dcf403602e23ffe8793bb69eedae2f7
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-08-12 16:36:40 +0000

    Thread: refix 6174
    [SVN r79980]

--- test/test_6174.cpp
+++ test/test_6174.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2010 Vicente Botet
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 
 
 #include <boost/thread.hpp>
@@ -16,13 +21,21 @@ private:
 public:
     MovableButNonCopyable() {};
     MovableButNonCopyable(MovableButNonCopyable&&) {};
-    MovableButNonCopyable& operator=(MovableButNonCopyable&&) {
+    MovableButNonCopyable& operator=(MovableButNonCopyable&&)
+    {
       return *this;
     };
 };
+
+MovableButNonCopyable construct()
+{
+  return MovableButNonCopyable();
+}
+
 int main()
 {
-    boost::packaged_task<MovableButNonCopyable>(MovableButNonCopyable());
+    boost::packaged_task<MovableButNonCopyable> pt(construct);
+    pt();
     return 0;
 }
 #else
