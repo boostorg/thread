460a38b994eae05fe48b5988ab429680dbec0d35
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-09-16 18:50:17 +0000

    Thread: (posix) Make use of BOOST_TRY family macros
    [SVN r80544]

--- include/boost/thread/detail/config.hpp
+++ include/boost/thread/detail/config.hpp
@@ -8,6 +8,11 @@
 #ifndef BOOST_THREAD_CONFIG_WEK01032003_HPP
 #define BOOST_THREAD_CONFIG_WEK01032003_HPP
 
+// Force SIG_ATOMIC_MAX to be defined
+#ifndef __STDC_LIMIT_MACROS
+#define __STDC_LIMIT_MACROS
+#endif
+
 #include <boost/config.hpp>
 #include <boost/detail/workaround.hpp>
 
