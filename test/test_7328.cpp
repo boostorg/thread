c47ea136b680df9b0620bc174e7e483c6f6cdc8b
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-10-29 19:19:40 +0000

    Thread Split lock.hpp + towards allowing to disable interruptions
    [SVN r81105]

--- test/test_7328.cpp
+++ test/test_7328.cpp
@@ -3,6 +3,8 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#define BOOST_THREAD_PROVIDES_INTERRUPTIONS
+
 #include <iostream>
 #include <boost/thread.hpp>
 #include <boost/detail/lightweight_test.hpp>
--- test/test_once.cpp
+++ test/test_once.cpp
@@ -4,6 +4,7 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_THREAD_VERSION 2
+#define BOOST_THREAD_PROVIDES_INTERRUPTIONS
 
 #include <boost/test/unit_test.hpp>
 #include <boost/thread/thread.hpp>
