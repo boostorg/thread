50ae9d93a68be5ad6bdd4ce7d2db95a917743f60
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-12-01 11:07:10 +0000

    Thread: Use BOOST_THREAD_LOG
    [SVN r81646]

--- test/test_7665.cpp
+++ test/test_7665.cpp
@@ -4,27 +4,36 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_THREAD_VERSION 2
+#define BOOST_THREAD_USES_LOG
 
 #include <iostream>
 #include <boost/thread.hpp>
+#include <boost/thread/detail/log.hpp>
 
 void thread()
 {
-  std::cout << "Sleeping for 10 seconds - change time\n";
+  BOOST_THREAD_LOG << "<thrd" << BOOST_THREAD_END_LOG;
+  try {
   boost::this_thread::sleep_for(boost::chrono::seconds(30));
-  std::cout << "Ended\n";
+  } catch (...)
+  {
+    BOOST_THREAD_LOG << "thrd exception" << BOOST_THREAD_END_LOG;
+    throw;
+  }
   //while (1)     ; // Never quit
+  BOOST_THREAD_LOG << "thrd>" << BOOST_THREAD_END_LOG;
 }
 
 boost::thread example(thread);
 
 int main()
 {
-  std::cout << "Main thread START\n";
+  BOOST_THREAD_LOG << "<main" << BOOST_THREAD_END_LOG;
   boost::this_thread::sleep_for(boost::chrono::seconds(30));
-  std::cout << "Main thread END\n";
+  BOOST_THREAD_LOG << "main" << BOOST_THREAD_END_LOG;
   //while (1)     ; // Never quit
   example.join();
+  BOOST_THREAD_LOG << "main>" << BOOST_THREAD_END_LOG;
   return 0;
 }
 
