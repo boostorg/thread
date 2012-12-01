d3f553652c876d8451271185028e3ec9a39e1b10
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2012-12-01 11:08:03 +0000

    Thread: force BOOST_THREAD_PROVIDES_GENERIC_SHARED_MUTEX_ON_WIN

--- test/test_4882.cpp
+++ test/test_4882.cpp
@@ -4,18 +4,19 @@
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_THREAD_VERSION 2
-#define BOOST_THREAD_USES_LOG
+#define BOOST_THREAD_PROVIDES_GENERIC_SHARED_MUTEX_ON_WIN
+//#define BOOST_THREAD_USES_LOG
 
 #include <boost/thread/thread.hpp>
 #include <boost/thread/shared_mutex.hpp>
 #include <boost/detail/no_exceptions_support.hpp>
-#include <boost/thread/detail/log.hpp>
+//#include <boost/thread/detail/log.hpp>
 
 boost::shared_mutex mutex;
 
 void thread()
 {
-  BOOST_THREAD_LOG << "<thrd" << BOOST_THREAD_END_LOG;
+  //BOOST_THREAD_LOG << "<thrd" << BOOST_THREAD_END_LOG;
   BOOST_TRY
   {
     for (int i =0; i<10; ++i)
@@ -25,40 +26,40 @@ void thread()
 
       if (mutex.timed_lock(timeout))
       {
-        BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
+        //BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
         boost::this_thread::sleep(boost::posix_time::milliseconds(10));
         mutex.unlock();
-        BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
+        //BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
       }
 #else
       boost::chrono::system_clock::time_point timeout = boost::chrono::system_clock::now() + boost::chrono::milliseconds(50);
 
-      BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
+      //BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
       if (mutex.try_lock_until(timeout))
       {
-        BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
+        //BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
         boost::this_thread::sleep_for(boost::chrono::milliseconds(10));
         mutex.unlock();
-        BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
+        //BOOST_THREAD_LOG << "<thrd" << " i="<<i << BOOST_THREAD_END_LOG;
       }
 #endif
     }
   }
   BOOST_CATCH (boost::lock_error& le)
   {
-    BOOST_THREAD_LOG << "lock_error exception thrd>" << BOOST_THREAD_END_LOG;
+    //BOOST_THREAD_LOG << "lock_error exception thrd>" << BOOST_THREAD_END_LOG;
   }
   BOOST_CATCH (...)
   {
-    BOOST_THREAD_LOG << "exception thrd>" << BOOST_THREAD_END_LOG;
+    //BOOST_THREAD_LOG << "exception thrd>" << BOOST_THREAD_END_LOG;
   }
   BOOST_CATCH_END
-  BOOST_THREAD_LOG << "thrd>" << BOOST_THREAD_END_LOG;
+  //BOOST_THREAD_LOG << "thrd>" << BOOST_THREAD_END_LOG;
 }
 
 int main()
 {
-  BOOST_THREAD_LOG << "<main" << BOOST_THREAD_END_LOG;
+  //BOOST_THREAD_LOG << "<main" << BOOST_THREAD_END_LOG;
   const int nrThreads = 20;
   boost::thread* threads[nrThreads];
 
@@ -68,10 +69,10 @@ int main()
   for (int i = 0; i < nrThreads; ++i)
   {
     threads[i]->join();
-    BOOST_THREAD_LOG << "main" << BOOST_THREAD_END_LOG;
+    //BOOST_THREAD_LOG << "main" << BOOST_THREAD_END_LOG;
     delete threads[i];
-    BOOST_THREAD_LOG << "main" << BOOST_THREAD_END_LOG;
+    //BOOST_THREAD_LOG << "main" << BOOST_THREAD_END_LOG;
   }
-  BOOST_THREAD_LOG << "main>" << BOOST_THREAD_END_LOG;
+  //BOOST_THREAD_LOG << "main>" << BOOST_THREAD_END_LOG;
   return 0;
 }
