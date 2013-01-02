574ddd7e7d244c62f1b02fe53be59a0f12405913
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2013-01-02 14:14:03 +0000

    Thread: complete externally_locked_stream.

--- example/not_interleaved.cpp
+++ example/not_interleaved.cpp
@@ -6,9 +6,10 @@
 
 // adapted from the example given by Howard Hinnant in
 
+#define BOOST_THREAD_VERSION 4
 
 #include <iostream>
-#include <boost/thread/thread.hpp>
+#include <boost/thread/scoped_thread.hpp>
 #include <boost/thread/externally_locked_stream.hpp>
 
 void use_cerr(boost::externally_locked_stream<std::ostream> &mcerr)
@@ -18,7 +19,18 @@ void use_cerr(boost::externally_locked_stream<std::ostream> &mcerr)
   while (chrono::steady_clock::now() < tf)
   {
     mcerr << "logging data to cerr\n";
-    this_thread::sleep_for(milliseconds(500));
+    this_thread::sleep_for(chrono::milliseconds(500));
+  }
+}
+
+void use_cout(boost::externally_locked_stream<std::ostream> &mcout)
+{
+  using namespace boost;
+  auto tf = chrono::steady_clock::now() + chrono::seconds(5);
+  while (chrono::steady_clock::now() < tf)
+  {
+    mcout << "logging data to cout\n";
+    this_thread::sleep_for(chrono::milliseconds(250));
   }
 }
 
@@ -26,17 +38,20 @@ int main()
 {
   using namespace boost;
 
-  externally_locked_stream<std::ostream> mcerr(std::cerr, terminal_mutex());
-  externally_locked_stream<std::ostream> mcout(std::cerr, terminal_mutex());
-  externally_locked_stream<std::istream> mcin(std::cerr, terminal_mutex());
+  recursive_mutex terminal_mutex;
 
-  thread t1(use_cerr, mcerr);
-  this_thread::sleep_for(boost::chrono::seconds(2));
+  externally_locked_stream<std::ostream> mcerr(std::cerr, terminal_mutex);
+  externally_locked_stream<std::ostream> mcout(std::cout, terminal_mutex);
+  externally_locked_stream<std::istream> mcin(std::cin, terminal_mutex);
+
+  scoped_thread<> t1(thread(use_cerr, boost::ref(mcerr)));
+  scoped_thread<> t2(thread(use_cout, boost::ref(mcout)));
+  this_thread::sleep_for(chrono::seconds(2));
   std::string nm;
   mcout << "Enter name: ";
-  mcin >> nm;
+  //mcin >> nm;
   t1.join();
   mcout << nm << '\n';
-  return 0;
+  return 1;
 }
 
