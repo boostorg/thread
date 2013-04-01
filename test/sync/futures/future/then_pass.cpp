460ee933b2a7c1962c0bf0d7ee16f64e83253261
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2013-04-01 16:17:05 +0000

    Thread: major rewrite of future::then.

--- test/sync/futures/future/then_pass.cpp
+++ test/sync/futures/future/then_pass.cpp
@@ -12,6 +12,7 @@
 
 #define BOOST_THREAD_VERSION 4
 #define BOOST_THREAD_DONT_PROVIDE_FUTURE_INVALID_AFTER_GET
+//#define BOOST_THREAD_DONT_PROVIDE_THREAD_DESTRUCTOR_CALLS_TERMINATE_IF_JOINABLE
 #define BOOST_THREAD_USES_LOG
 #define BOOST_THREAD_USES_LOG_THREAD_ID
 #include <boost/thread/detail/log.hpp>
@@ -23,61 +24,68 @@
 
 int p1()
 {
+  BOOST_THREAD_LOG << "p1 < " << BOOST_THREAD_END_LOG;
+  boost::this_thread::sleep_for(boost::chrono::milliseconds(500));
+  BOOST_THREAD_LOG << "p1 >"  << BOOST_THREAD_END_LOG;
   return 1;
 }
 
 int p2(boost::future<int>& f)
 {
-  return 2 * f.get();
+  BOOST_THREAD_LOG << "p2 <" << &f << BOOST_THREAD_END_LOG;
+  BOOST_TEST(f.valid());
+  int i = f.get();
+  boost::this_thread::sleep_for(boost::chrono::milliseconds(500));
+  BOOST_THREAD_LOG << "p2 <" << &f << BOOST_THREAD_END_LOG;
+  return 2 * i;
 }
 
 int main()
 {
-  BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-  boost::future<int> f1 = boost::async(boost::launch::async, p1);
-  BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-  boost::future<int> f2 = f1.then(p2);
-  BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-  try
   {
-    BOOST_TEST(f2.get()==2);
     BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-  }
-  catch (std::exception& ex)
-  {
-    BOOST_THREAD_LOG << "ERRORRRRR "<<ex.what() << "" << BOOST_THREAD_END_LOG;
-    BOOST_ASSERT(false);
-  }
-  catch (...)
-  {
-    BOOST_THREAD_LOG << " ERRORRRRR exception thrown" << BOOST_THREAD_END_LOG;
-    BOOST_ASSERT(false);
+    boost::future<int> f1 = boost::async(boost::launch::async, &p1);
+    BOOST_TEST(f1.valid());
+    boost::future<int> f2 = f1.then(&p2);
+    BOOST_TEST(f2.valid());
+    try
+    {
+      BOOST_TEST(f2.get()==2);
+    }
+    catch (std::exception& ex)
+    {
+      BOOST_THREAD_LOG << "ERRORRRRR "<<ex.what() << "" << BOOST_THREAD_END_LOG;
+      BOOST_TEST(false);
+    }
+    catch (...)
+    {
+      BOOST_THREAD_LOG << " ERRORRRRR exception thrown" << BOOST_THREAD_END_LOG;
+      BOOST_TEST(false);
+    }
   }
   BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-
-//  {
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    boost::future<int> f2 = boost::async(p1).then(p2);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    BOOST_TEST(f2.get()==2);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//  }
-//  {
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    boost::future<int> f1 = boost::async(p1);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    boost::future<int> f2 = f1.then(p2).then(p2);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    BOOST_TEST(f2.get()==4);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//  }
-//  {
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    boost::future<int> f2 = boost::async(p1).then(p2).then(p2);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//    BOOST_TEST(f2.get()==4);
-//    BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
-//  }
+  {
+    boost::future<int> f2 = boost::async(p1).then(&p2);
+    BOOST_TEST(f2.get()==2);
+  }
+  BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
+  {
+    boost::future<int> f1 = boost::async(p1);
+    boost::future<int> f21 = f1.then(&p2);
+    boost::future<int> f2= f21.then(&p2);
+    BOOST_TEST(f2.get()==4);
+  }
+  BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
+  {
+    boost::future<int> f1 = boost::async(p1);
+    boost::future<int> f2= f1.then(&p2).then(&p2);
+    BOOST_TEST(f2.get()==4);
+  }
+  BOOST_THREAD_LOG << BOOST_THREAD_END_LOG;
+  {
+    boost::future<int> f2 = boost::async(p1).then(&p2).then(&p2);
+    BOOST_TEST(f2.get()==4);
+  }
 
   return boost::report_errors();
 }
