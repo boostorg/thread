9fc6fec1eb3a14c008ab176600b94ce19cbca060
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2013-07-04 21:40:15 +0000

    Thread: Add return type to lambda to be portable on msvc10.
    [SVN r84953]

--- test/sync/mutual_exclusion/sync_bounded_queue/multi_thread_pass.cpp
+++ test/sync/mutual_exclusion/sync_bounded_queue/multi_thread_pass.cpp
@@ -3,7 +3,7 @@
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// <boost/thread/sync_queue.hpp>
+// <boost/thread/sync_bounded_queue.hpp>
 
 // class sync_queue<T>
 
@@ -16,7 +16,7 @@
 
 #define BOOST_THREAD_VERSION 4
 
-#include <boost/thread/sync_queue.hpp>
+#include <boost/thread/sync_bounded_queue.hpp>
 #include <boost/thread/future.hpp>
 #include <boost/thread/barrier.hpp>
 
@@ -24,10 +24,10 @@
 
 struct call_push
 {
-  boost::sync_queue<int> &q_;
+  boost::sync_bounded_queue<int> &q_;
   boost::barrier& go_;
 
-  call_push(boost::sync_queue<int> &q, boost::barrier &go) :
+  call_push(boost::sync_bounded_queue<int> &q, boost::barrier &go) :
     q_(q), go_(go)
   {
   }
@@ -40,12 +40,32 @@ struct call_push
   }
 };
 
+struct call_push_2
+{
+  boost::sync_bounded_queue<int> &q_;
+  boost::barrier& go_;
+  boost::barrier& end_;
+
+  call_push_2(boost::sync_bounded_queue<int> &q, boost::barrier &go, boost::barrier &end) :
+    q_(q), go_(go), end_(end)
+  {
+  }
+  typedef void result_type;
+  void operator()()
+  {
+    go_.count_down_and_wait();
+    q_.push(42);
+    end_.count_down_and_wait();
+
+  }
+};
+
 struct call_pull
 {
-  boost::sync_queue<int> &q_;
+  boost::sync_bounded_queue<int> &q_;
   boost::barrier& go_;
 
-  call_pull(boost::sync_queue<int> &q, boost::barrier &go) :
+  call_pull(boost::sync_bounded_queue<int> &q, boost::barrier &go) :
     q_(q), go_(go)
   {
   }
@@ -59,7 +79,7 @@ struct call_pull
 
 void test_concurrent_push_and_pull_on_empty_queue()
 {
-  boost::sync_queue<int> q;
+  boost::sync_bounded_queue<int> q(4);
 
   boost::barrier go(2);
 
@@ -103,7 +123,7 @@ void test_concurrent_push_and_pull_on_empty_queue()
 
 void test_concurrent_push_on_empty_queue()
 {
-  boost::sync_queue<int> q;
+  boost::sync_bounded_queue<int> q(4);
   const unsigned int n = 3;
   boost::barrier go(n);
   boost::future<void> push_done[n];
@@ -138,9 +158,55 @@ void test_concurrent_push_on_empty_queue()
   }
 }
 
+void test_concurrent_push_on_full_queue()
+{
+  const unsigned int size = 2;
+  boost::sync_bounded_queue<int> q(size);
+  const unsigned int n = 2*size;
+  boost::barrier go(n);
+  boost::barrier end(size+1);
+  boost::future<void> push_done[n];
+
+  try
+  {
+    for (unsigned int i =0; i< n; ++i)
+      push_done[i]=boost::async(boost::launch::async,
+#if ! defined BOOST_NO_CXX11_LAMBDAS
+        [&q,&go,&end]()
+        {
+          go.wait();
+          q.push(42);
+          end.wait();
+        }
+#else
+        call_push_2(q,go,end)
+#endif
+      );
+
+    end.wait();
+    BOOST_TEST(!q.empty());
+    BOOST_TEST(q.full());
+    for (unsigned int i =0; i< size; ++i)
+      BOOST_TEST_EQ(q.pull(), 42);
+    end.wait();
+
+    for (unsigned int i = 0; i < n; ++i)
+      push_done[i].get();
+
+    BOOST_TEST(!q.empty());
+    for (unsigned int i =0; i< size; ++i)
+      BOOST_TEST_EQ(q.pull(), 42);
+    BOOST_TEST(q.empty());
+
+  }
+  catch (...)
+  {
+    BOOST_TEST(false);
+  }
+}
 void test_concurrent_pull_on_queue()
 {
-  boost::sync_queue<int> q;
+  boost::sync_bounded_queue<int> q(4);
   const unsigned int n = 3;
   boost::barrier go(n);
 
@@ -178,6 +244,7 @@ int main()
 {
   test_concurrent_push_and_pull_on_empty_queue();
   test_concurrent_push_on_empty_queue();
+  test_concurrent_push_on_full_queue();
   test_concurrent_pull_on_queue();
 
   return boost::report_errors();
