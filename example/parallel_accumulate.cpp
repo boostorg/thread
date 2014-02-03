155ccfffbede44a45564ba6d9058157dde97716f
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2014-02-03 18:57:10 +0100

    fix copyright.

--- example/parallel_accumulate.cpp
+++ example/parallel_accumulate.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2012-2013 Vicente Botet
+// Copyright (C) 2014 Vicente Botet
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
