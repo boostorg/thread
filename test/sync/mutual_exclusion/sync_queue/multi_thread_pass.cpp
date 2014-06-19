1ac02d59b831bf083ff09525b58c2e7b2e07052d
Christian Manning <cmanning999@gmail.com>
2014-06-19 01:56:40 +0100

    Add some tests for sync_queue::wait_pull_front.
    This also updates the existing tests to only test the non-deprecated
    interface and factors out the async ops into reusable templates.

--- test/sync/mutual_exclusion/sync_queue/multi_thread_pass.cpp
+++ test/sync/mutual_exclusion/sync_queue/multi_thread_pass.cpp
@@ -15,6 +15,7 @@
 #endif
 
 #define BOOST_THREAD_VERSION 4
+#define BOOST_THREAD_QUEUE_DEPRECATE_OLD
 
 #include <boost/thread/sync_queue.hpp>
 #include <boost/thread/future.hpp>
@@ -22,38 +23,75 @@
 
 #include <boost/detail/lightweight_test.hpp>
 
-struct call_push
+template <typename ValueType, void(boost::sync_queue<ValueType>::*push_back)(const ValueType&)>
+struct call_push_back
 {
-  boost::sync_queue<int> *q_;
+  boost::sync_queue<ValueType> *q_;
   boost::barrier *go_;
 
-  call_push(boost::sync_queue<int> *q, boost::barrier *go) :
+  call_push_back(boost::sync_queue<ValueType> *q, boost::barrier *go) :
     q_(q), go_(go)
   {
   }
   typedef void result_type;
   void operator()()
   {
-    go_->count_down_and_wait();
-    q_->push(42);
-
+    go_->wait();
+    (q_->*push_back)(42);
   }
 };
 
-struct call_pull
+template <typename ValueType, boost::queue_op_status(boost::sync_queue<ValueType>::*push_back)(const ValueType&)>
+struct call_op_push_back
 {
-  boost::sync_queue<int> *q_;
+  boost::sync_queue<ValueType> *q_;
   boost::barrier *go_;
 
-  call_pull(boost::sync_queue<int> *q, boost::barrier *go) :
+  call_op_push_back(boost::sync_queue<ValueType> *q, boost::barrier *go) :
     q_(q), go_(go)
   {
   }
-  typedef int result_type;
-  int operator()()
+  typedef boost::queue_op_status result_type;
+  boost::queue_op_status operator()()
   {
-    go_->count_down_and_wait();
-    return q_->pull();
+    go_->wait();
+    return (q_->*push_back)(42);
+  }
+};
+
+template <typename ValueType, ValueType(boost::sync_queue<ValueType>::*pull_front)()>
+struct call_pull_front
+{
+  boost::sync_queue<ValueType> *q_;
+  boost::barrier *go_;
+
+  call_pull_front(boost::sync_queue<ValueType> *q, boost::barrier *go) :
+    q_(q), go_(go)
+  {
+  }
+  typedef ValueType result_type;
+  ValueType operator()()
+  {
+    go_->wait();
+    return (q_->*pull_front)();
+  }
+};
+
+template <typename ValueType, boost::queue_op_status(boost::sync_queue<ValueType>::*pull_front)(ValueType&)>
+struct call_op_pull_front
+{
+  boost::sync_queue<ValueType> *q_;
+  boost::barrier *go_;
+
+  call_op_pull_front(boost::sync_queue<ValueType> *q, boost::barrier *go) :
+    q_(q), go_(go)
+  {
+  }
+  typedef boost::queue_op_status result_type;
+  boost::queue_op_status operator()(ValueType& v)
+  {
+    go_->wait();
+    return (q_->*pull_front)(v);
   }
 };
 
@@ -69,30 +107,72 @@ void test_concurrent_push_and_pull_on_empty_queue()
   try
   {
     push_done=boost::async(boost::launch::async,
-#if ! defined BOOST_NO_CXX11_LAMBDAS
-        [&q,&go]()
-        {
-          go.wait();
-          q.push(42);
-        }
-#else
-        call_push(&q,&go)
-#endif
-    );
+                           call_push_back<int, &boost::sync_queue<int>::push_back>(&q,&go));
     pull_done=boost::async(boost::launch::async,
-#if ! defined BOOST_NO_CXX11_LAMBDAS
-        [&q,&go]() -> int
-        {
-          go.wait();
-          return q.pull();
-        }
-#else
-        call_pull(&q,&go)
-#endif
-    );
+                           call_pull_front<int, &boost::sync_queue<int>::pull_front>(&q,&go));
 
     push_done.get();
     BOOST_TEST_EQ(pull_done.get(), 42);
+    BOOST_TEST(q.empty());
+  }
+  catch (...)
+  {
+    BOOST_TEST(false);
+  }
+}
+
+void test_concurrent_push_and_wait_pull_on_empty_queue()
+{
+  boost::sync_queue<int> q;
+  const unsigned int n = 3;
+  boost::barrier go(n);
+
+  boost::future<boost::queue_op_status> pull_done[n];
+  int results[n];
+
+  try
+  {
+    for (unsigned int i =0; i< n; ++i)
+      pull_done[i]=boost::async(boost::launch::async,
+                                call_op_pull_front<int, &boost::sync_queue<int>::wait_pull_front>(&q,&go),
+                                boost::ref(results[i]));
+
+    for (unsigned int i =0; i< n; ++i)
+      q.push_back(42);
+
+    for (unsigned int i = 0; i < n; ++i) {
+      BOOST_TEST(pull_done[i].get() == boost::queue_op_status::success);
+      BOOST_TEST_EQ(results[i], 42);
+    }
+    BOOST_TEST(q.empty());
+  }
+  catch (...)
+  {
+    BOOST_TEST(false);
+  }
+}
+
+void test_concurrent_wait_pull_and_close_on_empty_queue()
+{
+  boost::sync_queue<int> q;
+  const unsigned int n = 3;
+  boost::barrier go(n);
+
+  boost::future<boost::queue_op_status> pull_done[n];
+  int results[n];
+
+  try
+  {
+    for (unsigned int i =0; i< n; ++i)
+      pull_done[i]=boost::async(boost::launch::async,
+                                call_op_pull_front<int, &boost::sync_queue<int>::wait_pull_front>(&q,&go),
+                                boost::ref(results[i]));
+
+    q.close();
+
+    for (unsigned int i = 0; i < n; ++i) {
+      BOOST_TEST(pull_done[i].get() == boost::queue_op_status::closed);
+    }
     BOOST_TEST(q.empty());
   }
   catch (...)
@@ -112,23 +192,14 @@ void test_concurrent_push_on_empty_queue()
   {
     for (unsigned int i =0; i< n; ++i)
       push_done[i]=boost::async(boost::launch::async,
-#if ! defined BOOST_NO_CXX11_LAMBDAS
-        [&q,&go]()
-        {
-          go.wait();
-          q.push(42);
-        }
-#else
-        call_push(&q,&go)
-#endif
-    );
+                                call_push_back<int, &boost::sync_queue<int>::push_back>(&q,&go));
 
     for (unsigned int i = 0; i < n; ++i)
       push_done[i].get();
 
     BOOST_TEST(!q.empty());
     for (unsigned int i =0; i< n; ++i)
-      BOOST_TEST_EQ(q.pull(), 42);
+      BOOST_TEST_EQ(q.pull_front(), 42);
     BOOST_TEST(q.empty());
 
   }
@@ -149,20 +220,11 @@ void test_concurrent_pull_on_queue()
   try
   {
     for (unsigned int i =0; i< n; ++i)
-      q.push(42);
+      q.push_back(42);
 
     for (unsigned int i =0; i< n; ++i)
       pull_done[i]=boost::async(boost::launch::async,
-#if ! defined BOOST_NO_CXX11_LAMBDAS
-        [&q,&go]() -> int
-        {
-          go.wait();
-          return q.pull();
-        }
-#else
-        call_pull(&q,&go)
-#endif
-    );
+                                call_pull_front<int, &boost::sync_queue<int>::pull_front>(&q,&go));
 
     for (unsigned int i = 0; i < n; ++i)
       BOOST_TEST_EQ(pull_done[i].get(), 42);
@@ -179,6 +241,8 @@ int main()
   test_concurrent_push_and_pull_on_empty_queue();
   test_concurrent_push_on_empty_queue();
   test_concurrent_pull_on_queue();
+  test_concurrent_push_and_wait_pull_on_empty_queue();
+  test_concurrent_wait_pull_and_close_on_empty_queue();
 
   return boost::report_errors();
 }
