f0d0c0551b41b45d812c3ff44e5771b866c0fafe
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2014-09-29 00:00:49 +0200

    rollback to not define BOOST_RESULT_OF_USE_DECLTYPE.

--- example/parallel_quick_sort.cpp
+++ example/parallel_quick_sort.cpp
@@ -8,7 +8,7 @@
 #define BOOST_THREAD_USES_LOG_THREAD_ID
 #define BOOST_THREAD_QUEUE_DEPRECATE_OLD
 #if ! defined  BOOST_NO_CXX11_DECLTYPE
-#define BOOST_RESULT_OF_USE_DECLTYPE
+//#define BOOST_RESULT_OF_USE_DECLTYPE
 #endif
 
 #include <boost/thread/executors/basic_thread_pool.hpp>
