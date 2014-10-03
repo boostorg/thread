58cde5af05535dd8ebc448f7a53a667c13c28d85
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2014-10-03 21:46:57 +0200

    Trying to use std::function for executors::work. This doesn't works yet.

--- include/boost/thread/executors/work.hpp
+++ include/boost/thread/executors/work.hpp
@@ -7,6 +7,15 @@
 #ifndef BOOST_THREAD_EXECUTORS_WORK_HPP
 #define BOOST_THREAD_EXECUTORS_WORK_HPP
 
+#include <boost/thread/detail/config.hpp>
+
+#if ! defined BOOST_THREAD_EXECUTORS_WORK_ACCEPTS_MOVABLE \
+ && ! defined BOOST_THREAD_EXECUTORS_WORK_DONT_ACCEPT_MOVABLE
+#define BOOST_THREAD_EXECUTORS_WORK_ACCEPTS_MOVABLE
+//#define BOOST_THREAD_EXECUTORS_WORK_DONT_ACCEPT_MOVABLE
+#endif
+
+#if defined BOOST_THREAD_EXECUTORS_WORK_ACCEPTS_MOVABLE
 
 #include <boost/thread/detail/nullary_function.hpp>
 
@@ -17,5 +26,18 @@ namespace boost
     typedef detail::nullary_function<void()> work;
   }
 } // namespace boost
+
+#else
+#include <boost/thread/csbl/functional.hpp>
+
+namespace boost
+{
+  namespace executors
+  {
+    typedef csbl::function<void()> work;
+  }
+} // namespace boost
+
+#endif
 
 #endif //  BOOST_THREAD_EXECUTORS_WORK_HPP
