cbf9fe8a5c07697061cb822b61bab49417e59d98
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2014-11-02 15:08:44 +0100

    Fix inspection issues.

--- test/winrt_init.cpp
+++ test/winrt_init.cpp
@@ -1,3 +1,5 @@
+// Copyright (C) 2014 Vicente Botet
+//
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
