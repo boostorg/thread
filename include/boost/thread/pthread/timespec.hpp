c7348b29cf8bfa1272645d04784419d37e1e7db5
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-02-25 22:41:00 +0100

    Compile conditionally to try to fix the timespec monotonic issue. This is not a final solution as we need to add two defines while building Boost.Thread, but atleast will allow to check if the solution work.

--- include/boost/thread/pthread/timespec.hpp
+++ include/boost/thread/pthread/timespec.hpp
@@ -75,7 +75,7 @@ namespace boost
     {
       timespec ts;
 
-#ifdef BOOST_THREAD_TIMESPEC_CLOCK_MONOTONIC
+#ifdef CLOCK_MONOTONIC
       if ( ::clock_gettime( CLOCK_MONOTONIC, &ts ) )
       {
         ts.tv_sec = 0;
@@ -98,21 +98,25 @@ namespace boost
       return ts;
     }
 
-#ifdef CLOCK_MONOTONIC
-    inline timespec timespec_now_monotonic()
+    inline timespec timespec_now_realtime()
     {
       timespec ts;
 
-      if ( ::clock_gettime( CLOCK_MONOTONIC, &ts ) )
+#if defined(BOOST_THREAD_TIMESPEC_MAC_API)
+      timeval tv;
+      ::gettimeofday(&tv, 0);
+      ts.tv_sec = tv.tv_sec;
+      ts.tv_nsec = tv.tv_usec * 1000;
+#else
+      if ( ::clock_gettime( CLOCK_REALTIME, &ts ) )
       {
         ts.tv_sec = 0;
         ts.tv_nsec = 0;
         BOOST_ASSERT(0 && "Boost::Thread - Internal Error");
       }
+#endif
       return ts;
     }
-#endif
-
     inline timespec timespec_zero()
     {
       timespec ts;
