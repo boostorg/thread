c969a6cf9c45366c6c3d39c5e061a40ddd721363
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-02-28 08:22:15 +0100

    Fix uninitialized variables in scoped thread examples. Disable detach example for clang std::thread, it crashes.

--- example/std_scoped_thread.cpp
+++ example/std_scoped_thread.cpp
@@ -16,6 +16,7 @@ int main()
 #include <iostream>
 #include <boost/thread/scoped_thread.hpp>
 #include <thread>
+#include <cassert>
 
 void do_something(int& i)
 {
@@ -47,53 +48,60 @@ void do_something_in_current_thread()
 {
 }
 
-//void do_something_with_current_thread(std::thread&& th)
-//{
-//  th.join();
-//}
 using strict_scoped_thread = boost::strict_scoped_thread<boost::join_if_joinable, std::thread>;
 using scoped_thread = boost::scoped_thread<boost::join_if_joinable, std::thread>;
 
 int main()
 {
   {
-    int some_local_state;
+    int some_local_state=0;
     strict_scoped_thread t( (std::thread(func(some_local_state))));
 
     do_something_in_current_thread();
   }
   {
-    int some_local_state;
+    int some_local_state=0;
     std::thread t(( func(some_local_state) ));
     strict_scoped_thread g( (boost::move(t)) );
 
     do_something_in_current_thread();
   }
-//  {
-//    int some_local_state;
-//    std::thread t(( func(some_local_state) ));
-//    strict_scoped_thread g( (boost::move(t)) );
-//
-//    do_something_in_current_thread();
-//    do_something_with_current_thread(std::thread(g));
-//  }
   {
-    int some_local_state;
-    scoped_thread t( (std::thread(func(some_local_state))));
-
-    if (t.joinable())
-      t.join();
-    else
-      do_something_in_current_thread();
-  }
-  {
-    int some_local_state;
+    int some_local_state=0;
     std::thread t(( func(some_local_state) ));
-    scoped_thread g( (std::move(t)) );
-    g.detach();
+    strict_scoped_thread g( (std::move(t)) );
 
     do_something_in_current_thread();
   }
+  {
+    int some_local_state=1;
+    scoped_thread t( (std::thread(func(some_local_state))));
+
+    if (t.joinable()) {
+      t.join();
+      assert( ! t.joinable() );
+    }
+    else
+      do_something_in_current_thread();
+  }
+#if ! defined __clang__
+  try
+  {
+    int some_local_state=1;
+    std::thread t(( func(some_local_state) ));
+    scoped_thread g( (boost::move(t)) );
+    if (g.joinable()) {
+      // CLANG crash here
+      g.detach();
+      assert( ! g.joinable() );
+    }
+
+    do_something_in_current_thread();
+  }
+  catch (...) {
+    assert( false);
+  }
+#endif
   {
     scoped_thread g( &f, 1, 2 );
     do_something_in_current_thread();
