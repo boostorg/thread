ace2b8f89ec398654e9f696d26accbb0b8039bdb
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2017-05-06 23:04:52 +0200

    rollback and use RAII.

--- include/boost/thread/win32/condition_variable.hpp
+++ include/boost/thread/win32/condition_variable.hpp
@@ -138,37 +138,37 @@ namespace boost
                 detail::win32::ReleaseSemaphore(wake_sem,count_to_wake,0);
             }
 
-//            template<typename lock_type>
-//            struct relocker
-//            {
-//                BOOST_THREAD_NO_COPYABLE(relocker)
-//                lock_type& _lock;
-//                bool _unlocked;
-//
-//                relocker(lock_type& lock_):
-//                    _lock(lock_), _unlocked(false)
-//                {}
-//                void unlock()
-//                {
-//                  if ( ! _unlocked )
-//                  {
-//                    _lock.unlock();
-//                    _unlocked=true;
-//                  }
-//                }
-//                void lock()
-//                {
-//                  if ( _unlocked )
-//                  {
-//                    _lock.lock();
-//                    _unlocked=false;
-//                  }
-//                }
-//                ~relocker() BOOST_NOEXCEPT_IF(false)
-//                {
-//                  lock();
-//                }
-//            };
+            template<typename lock_type>
+            struct relocker
+            {
+                BOOST_THREAD_NO_COPYABLE(relocker)
+                lock_type& _lock;
+                bool _unlocked;
+
+                relocker(lock_type& lock_):
+                    _lock(lock_), _unlocked(false)
+                {}
+                void unlock()
+                {
+                  if ( ! _unlocked )
+                  {
+                    _lock.unlock();
+                    _unlocked=true;
+                  }
+                }
+                void lock()
+                {
+                  if ( _unlocked )
+                  {
+                    _lock.lock();
+                    _unlocked=false;
+                  }
+                }
+                ~relocker() BOOST_NOEXCEPT_IF(false)
+                {
+                  lock();
+                }
+            };
 
 
             entry_ptr get_wait_entry()
@@ -194,65 +194,59 @@ namespace boost
                 }
             }
 
-//            struct entry_manager
-//            {
-//                entry_ptr const entry;
-//                boost::mutex& internal_mutex;
-//
-//                BOOST_THREAD_NO_COPYABLE(entry_manager)
-//                entry_manager(entry_ptr const& entry_, boost::mutex& mutex_):
-//                    entry(entry_), internal_mutex(mutex_)
-//                {}
-//
-//                ~entry_manager() BOOST_NOEXCEPT_IF(false)
-//                {
-//                    boost::lock_guard<boost::mutex> internal_lock(internal_mutex);
-//                    entry->remove_waiter();
-//                }
-//
-//                list_entry* operator->()
-//                {
-//                    return entry.get();
-//                }
-//            };
+            struct entry_manager
+            {
+                entry_ptr const entry;
+                boost::mutex& internal_mutex;
+
+
+                BOOST_THREAD_NO_COPYABLE(entry_manager)
+                entry_manager(entry_ptr const& entry_, boost::mutex& mutex_):
+                    entry(entry_), internal_mutex(mutex_)
+                {}
+
+                void remove_waiter()
+                {
+                  if (entry) {
+                    boost::lock_guard<boost::mutex> internal_lock(internal_mutex);
+                    entry->remove_waiter();
+                    entry = 0;
+                  }
+                }
+                ~entry_manager() BOOST_NOEXCEPT_IF(false)
+                {
+                    remove_waiter();
+                }
+
+                list_entry* operator->()
+                {
+                    return entry.get();
+                }
+            };
 
 
         protected:
             template<typename lock_type>
             bool do_wait(lock_type& lock,timeout abs_time)
             {
-              //relocker<lock_type> locker(lock);
-              //entry_manager entry(get_wait_entry(), internal_mutex);
-              entry_ptr const entry = get_wait_entry();
-              try {
-                lock.unlock();
+              relocker<lock_type> locker(lock);
+              entry_manager entry(get_wait_entry(), internal_mutex);
+              locker.unlock();
 
-                bool woken=false;
-                while(!woken)
-                {
-                    if(!entry->wait(abs_time))
-                    {
-                        {
-                          boost::lock_guard<boost::mutex> internal_lock(internal_mutex);
-                          entry->remove_waiter();
-                        }
-                        lock.lock();
-                        return false;
-                    }
-                    woken=entry->woken();
-                }
-                {
-                  boost::lock_guard<boost::mutex> internal_lock(internal_mutex);
-                  entry->remove_waiter();
-                }
-                lock.lock();
-                return woken;
-              }
-              catch (...)
+              bool woken=false;
+              while(!woken)
               {
-                lock.lock();
-                throw;
+                  if(!entry->wait(abs_time))
+                  {
+                      return false;
+                  }
+
+                  woken=entry->woken();
               }
+              // do it here to avoid throwing on the destructor
+              entry->remove_waiter();
+              locker.lock();
+              return woken;
             }
 
             template<typename lock_type,typename predicate_type>
