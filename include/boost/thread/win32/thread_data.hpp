10e408d1e922bfca17350bfe13b7f28311158538
Nikita Kniazev <nok.raven@gmail.com>
2018-10-04 00:29:20 +0300

    Simplify thread specific storage

--- include/boost/thread/win32/thread_data.hpp
+++ include/boost/thread/win32/thread_data.hpp
@@ -78,16 +78,7 @@ namespace boost
         struct shared_state_base;
         struct tss_cleanup_function;
         struct thread_exit_callback_node;
-        struct tss_data_node
-        {
-            boost::shared_ptr<boost::detail::tss_cleanup_function> func;
-            void* value;
-
-            tss_data_node(boost::shared_ptr<boost::detail::tss_cleanup_function> func_,
-                          void* value_):
-                func(func_),value(value_)
-            {}
-        };
+        typedef boost::shared_ptr<void> tss_data_node;
 
         struct thread_data_base;
         void intrusive_ptr_add_ref(thread_data_base * p);
